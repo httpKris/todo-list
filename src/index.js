e53dc96ad3e53d7e66e2bf4aadc65807ffbe95ef
httpKris <kira7999@gmail.com>
2020-12-05 13:12:18 +0300

    added for loop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 let list = document.querySelector('.todo-list')
-let listItem = document.querySelector('.todo-list-item')
+let listItem = document.querySelectorAll('.todo-list-item')
 let input = document.querySelector('.todo-input')
 let form = document.querySelector('.todo-form')
 let priority = document.querySelector('.todo-priority')
+let charCounter = document.querySelector('.char-counter');
+let button = document.querySelector('.todo-add')
 
-list.onclick = function () {
-	listItem.classList.toggle('is-important')
-}
 
 priority.onclick = function () {
 	priority.classList.toggle('is-important')
@@ -23,7 +22,27 @@ form.onsubmit = function (event) {
 	newListItem.textContent = input.value
 	list.append(newListItem)
 	input.value = ''
+	charCounter.textContent = 0
 	if (priority.classList.contains('is-important')) {
 		newListItem.classList.add('is-important')
+	}
+}
+input.oninput = function () {
+	charCounter.textContent = input.value.length
+	if (input.value.length > 20) {
+		charCounter.classList.add('warning')
+		button.disabled = true
+		button.classList.add('warningButton')
+	} else {
+		charCounter.classList.remove('warning')
+		button.classList.remove('warningButton')
+		button.disabled = false
+	}
+}
+
+for (let element of listItem) {
+	// console.log(element)
+	element.onclick = function () {
+		element.classList.toggle('is-important')
 	}
 }
